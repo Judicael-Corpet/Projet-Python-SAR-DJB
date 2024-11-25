40346a53647ce975dad0151864ecf9c09aeda8cb
Dan Calamia <calamia.dan@gmail.com>
2024-11-25 18:12:35 +0100

    Arrière plan Joueur modif

--- player.py
+++ player.py
@@ -28,7 +28,7 @@ class Player(pygame.sprite.Sprite):
         self.sprite_sheet=pygame.image.load('Marvel.png') 
         self.image=self.get_image(95,0) # get image in this coordinate
         self.image=pygame.transform.scale(self.image,(32,32))
-        self.image.set_colorkey([0,0,0]) # to remove the withe color of the background
+        self.image.set_colorkey([255,255,255]) # to remove the withe color of the background
         self.rect=pygame.Rect(self.x,self.y,self.size[0],self.size[1]) # create a rectangle for the player, pygame.Rect() --> create a rectangle object
         
         # add the player 2 team 1
