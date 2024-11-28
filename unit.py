4a698ceab7717c4b02076c21a2f58648e00e70b7
Judicael Corpet <judicael.corpet@gmail.com>
2024-11-28 23:46:59 +0100

    verifications main

--- unit.py
+++ unit.py
@@ -74,7 +74,7 @@ class Unit(pygame.sprite.Sprite):
             self.sprite_sheet = pygame.image.load('personnages/avengers.png')
             self.image = self.get_image(0,0) # get image in this coordinate
             self.image = pygame.transform.scale(self.image,self.size)
-            #self.image.set_colorkey([0,0,0]) # to remove the withe color of the background
+            self.image.set_colorkey([0,0,0]) # to remove the withe color of the background
             self.rect = pygame.Rect(self.x,self.y,self.size[0],self.size[1]) # create a rectangle for the player, pygame.Rect() --> create a rectangle object
         
 
