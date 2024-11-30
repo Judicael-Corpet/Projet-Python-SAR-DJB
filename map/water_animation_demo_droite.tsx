b9c59849c63579e03affffbb648d8d02d9b9f390
R-Baptiste <baptiste.rouquette@outlook.com>
2024-11-30 16:37:52 +0100

    Assemblement 2

---
+++ map/water_animation_demo_droite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="water_animation_demo_droite" tilewidth="32" tileheight="32" tilecount="24" columns="4">
+ <image source="water_animation_demo_droite.png" width="128" height="192"/>
+</tileset>
